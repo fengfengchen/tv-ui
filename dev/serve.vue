1192ff57e5c80c327ef5d33009749b8362525a7d
zengmeng <zengmeng@haohandata.com.cn>
2021-10-19 15:23:25 +0800

    添加一个按钮组件

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,19 @@
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 // Uncomment import and local "components" registration if library is not registered globally.
-// import { TvUiSample } from '@/entry.esm';
+import { FccButton } from "@/entry.esm";
 
 export default Vue.extend({
-  name: 'ServeDev',
-  // components: {
-  //  TvUiSample,
-  // }
+  name: "ServeDev",
+  components: {
+    FccButton,
+  },
 });
 </script>
 
 <template>
   <div id="app">
     <tv-ui-sample />
+    <FccButton text="i'm your father"></FccButton>
   </div>
 </template>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
-export { default as TvUiSample } from './tv-ui-sample.vue';
+export { default as TvUiSample } from "./tv-ui-sample.vue";
+export { default as FccButton } from "./fcc-button.vue";
