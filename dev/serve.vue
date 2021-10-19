acbecf8ca065f8413589cc54cba96cf97ac55f89
zengmeng <zengmeng@haohandata.com.cn>
2021-10-19 14:45:52 +0800

    src-init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TvUiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  TvUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <tv-ui-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TvUiSample } from './tv-ui-sample.vue';
